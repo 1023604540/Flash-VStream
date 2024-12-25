92b7eaee4309c6921702d0abb17e84cc60b47d20
Ziqin Zhang <ziqin.zhang@tum.de>
2024-12-25 17:22:08 +0100

    debug

--- flash_vstream/model/recurrent_memory.py
+++ flash_vstream/model/recurrent_memory.py
@@ -252,6 +252,7 @@ class TransformerProjector(nn.Module):
         # ensure hidden_states and read_memories are on the same device
         device = hidden_states.device
         read_memories = read_memories.to(device)
+        attention_mask = attention_mask.to(device)
 
         hidden_states, ps = pack([read_memories, hidden_states], 'b * d')  # shape: [B, num_memory_tokens + seq_length, D]
         print("after pack", hidden_states.shape)
