c0de5c90fa74a4424d2a4a8e497db3c03148c810
Ziqin Zhang <ziqin.zhang@tum.de>
2024-12-25 21:41:32 +0100

    debug

--- flash_vstream/model/vstream_arch.py
+++ flash_vstream/model/vstream_arch.py
@@ -489,8 +489,8 @@ class VStreamMetaForCausalLM(ABC):
                     cur_memory, long_memory_compreesed, Turing_memory_compreesed, _ = self.video_embedding_memory   # for streaming mode, input is processed by cli_video_stream.py
                     logger.info(f'Read cur_memory={cur_memory.shape} {cur_memory.dtype}, long_memory_compreesed={long_memory_compreesed.shape} {long_memory_compreesed.dtype}, Turing_memory_compreesed={Turing_memory_compreesed.shape} {Turing_memory_compreesed.dtype}')
                     image_feature = torch.cat([Turing_memory_compreesed.flatten(0, 1), long_memory_compreesed.flatten(0, 1), cur_memory.flatten(0, 1)], dim=0)
-                    print("chunk_flag in streaming ", self.chunk_flag)
-                    if self.chunk_flag:
+                    print("chunk_flag in streaming ", self.chunk_flag.value)
+                    if self.chunk_flag.value:
                         print("flag triggered")
                         image_feature = image_feature.to(self.device)
                         self.recurrent_memory_transformer = self.recurrent_memory_transformer.to(self.device)
