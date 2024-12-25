e171647ebec857c4f035eace3f00435c153cce1b
Ziqin Zhang <ziqin.zhang@tum.de>
2024-12-25 18:16:01 +0100

    debug

--- flash_vstream/model/vstream_arch.py
+++ flash_vstream/model/vstream_arch.py
@@ -147,7 +147,6 @@ class VStreamMetaForCausalLM(ABC):
         # support video streaming mode
         # load recurrent memory 
         self.recurrent_memory_transformer = TransformerProjector()
-        self.recurrent_memory_transformer.to(self.device)
         self.recurrent_memory = None
         self.use_video_streaming_mode = False
         self.video_embedding_memory = None  # set to torch.multiprocessing.Manager.list() when launching
@@ -486,7 +485,8 @@ class VStreamMetaForCausalLM(ABC):
                     logger.info(f'Read cur_memory={cur_memory.shape} {cur_memory.dtype}, long_memory_compreesed={long_memory_compreesed.shape} {long_memory_compreesed.dtype}, Turing_memory_compreesed={Turing_memory_compreesed.shape} {Turing_memory_compreesed.dtype}')
                     image_feature = torch.cat([Turing_memory_compreesed.flatten(0, 1), long_memory_compreesed.flatten(0, 1), cur_memory.flatten(0, 1)], dim=0)  # [n, 1024]
                     # if self.chunk_flag:
-                    image_feature.to(self.device)
+                    image_feature = image_feature.to(self.device)
+                    self.recurrent_memory_transformer = self.recurrent_memory_transformer.to(self.device)
                     self.recurrent_memory, _ = self.recurrent_memory_transformer.forward(image_feature, self.recurrent_memory)
                     print("recurrent_memory", self.recurrent_memory.shape)
                     print("cur_memory", cur_memory.shape)
