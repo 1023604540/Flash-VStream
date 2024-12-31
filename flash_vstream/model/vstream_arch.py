8acb3be3c2a5030e947f9747cf743014fe7082f9
Ziqin Zhang <ziqin.zhang@tum.de>
2024-12-31 14:26:39 +0100

    combine recurrent memory with STAR

--- flash_vstream/model/vstream_arch.py
+++ flash_vstream/model/vstream_arch.py
@@ -487,6 +487,11 @@ class VStreamMetaForCausalLM(ABC):
             try:
                 with self.video_embedding_mem_lock:
                     cur_memory, long_memory_compreesed, Turing_memory_compreesed, _ = self.video_embedding_memory   # for streaming mode, input is processed by cli_video_stream.py
+                    recurrent_memory = self.recurrent_memory[0] if len(self.recurrent_memory) != 0 else None
+                    if recurrent_memory == None:
+                        print("recurrent_memory is None")
+                    else:
+                        print("recurrent_memory", recurrent_memory.shape)
                     logger.info(f'Read cur_memory={cur_memory.shape} {cur_memory.dtype}, long_memory_compreesed={long_memory_compreesed.shape} {long_memory_compreesed.dtype}, Turing_memory_compreesed={Turing_memory_compreesed.shape} {Turing_memory_compreesed.dtype}')
                     image_feature = torch.cat([Turing_memory_compreesed.flatten(0, 1), long_memory_compreesed.flatten(0, 1), cur_memory.flatten(0, 1)], dim=0)
                     # if self.chunk_flag.value:
