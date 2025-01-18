2aaa70a5375bdec1b0656e93a6a44fb7ff6c6e65
Ziqin Zhang <ziqin.zhang@tum.de>
2025-01-18 17:03:34 +0100

    modify openai api call

--- flash_vstream/eval_video/eval_activitynet_qa.py
+++ flash_vstream/eval_video/eval_activitynet_qa.py
@@ -25,24 +25,20 @@ def parse_args():
     return args
 
 
-def annotate(prediction_set, caption_files, output_dir,args):
-    """
-    Evaluates question and answer pairs using GPT-3
-    Returns a score for correctness.
-    """
-    for file in tqdm(caption_files):
-        logging.info("Processing file: %s", file)
-        key = file[:-5] # Strip file extension
+def prepare_batch_file(prediction_set, caption_files, output_dir):
+    batch_requests = []
+    for idx, file in enumerate(tqdm(caption_files)):
+        key = file[:-5]  # Strip file extension
         qa_set = prediction_set[key]
         question = qa_set['q']
         answer = qa_set['a']
         pred = qa_set['pred']
         try:
-            # Compute the correctness score
-            openai = OpenAI(api_key=args.api_key)
-            completion = openai.chat.completions.create(
-                model="gpt-3.5-turbo",
-                messages=[
+            # Prepare the request
+            request = {"custom_id": key, "method": "POST", "url": "/v1/chat/completions", "body":
+                {
+                "model": "gpt-3.5-turbo",
+                "messages": [
                     {
                         "role": "system",
                         "content": 
@@ -67,20 +63,15 @@ def annotate(prediction_set, caption_files, output_dir,args):
                             "For example, your response should look like this: {'pred': 'yes', 'score': 4.8}."
                     }
                 ],
-                temperature=0.002
-            )
-            # Convert response to a Python dictionary.
-            response_message = completion.choices[0].message.content
-            response_dict = ast.literal_eval(response_message)
-            result_qa_pair = [response_dict, qa_set]
-            # Save the question-answer pairs to a json file.
-            with open(f"{output_dir}/{key}.json", "w") as f:
-                json.dump(result_qa_pair, f)
-            sleep(0.5)
+                "temperature": 0.002
+            }}
+            batch_requests.append(request)
 
-        except Exception as e:
-            print(f"Error processing file '{key}': {e}")
-            sleep(1)
+        batch_file_path = os.path.join(output_dir, "batchinput.jsonl")
+        with open(batch_file_path, "w") as f:
+            for request in batch_requests:
+                f.write(json.dumps(request) + "\n")
+        return batch_file_path
 
 
 def main():
@@ -136,150 +127,133 @@ def main():
         qa_set = {"q": question, "a": answer, "pred": pred, "a_type": sample['answer_type'] if 'answer_type' in sample else None}
         prediction_set[id] = qa_set
 
-    num_tasks = args.num_tasks
+    batch_file_path = prepare_batch_file(prediction_set, caption_files, output_dir)
+    # Upload the batch file
+    client = OpenAI(api_key=args.api_key)
+    batch_input_file = client.files.create(
+        file=open(batch_file_path, "rb"),
+        purpose="batch"
+    )
 
-    # While loop to ensure that all captions are processed.
-    incomplete_lengths = []
-    for _ in range(100):
-        try:
-            # Files that have not been processed yet.
-            completed_files = os.listdir(output_dir)
-            print(f"completed_files: {len(completed_files)}")
+    # Create the batch
+    batch_input_file_id = batch_input_file.id
+    batch = client.batches.create(
+        input_file_id=batch_input_file_id,
+        endpoint="/v1/chat/completions",
+        completion_window="24h",
+        metadata={"description": "eval job"}
+    )
 
-            # Files that have not been processed yet.
-            incomplete_files = [f for f in caption_files if f not in completed_files]
-            print(f"incomplete_files: {len(incomplete_files)}")
-            incomplete_lengths.append(len(incomplete_files))
-            if len(incomplete_lengths) > 5 and len(set(incomplete_lengths[-5:])) <= 1:
-                print(f"incomplete_lengths: {incomplete_lengths}")
-                print(f"incomplete_files: {incomplete_files}")
-                print(f"completed_files: {completed_files}")
-                print(f"failed for 5 times, break")
-                break
-            # Break the loop when there are no incomplete files
-            if len(incomplete_files) == 0:
-                break
-            if len(incomplete_files) <= num_tasks:
-                num_tasks = 1
-
-            # Split tasks into parts.
-            part_len = len(incomplete_files) // num_tasks
-            all_parts = [incomplete_files[i:i + part_len] for i in range(0, len(incomplete_files), part_len)]
-            task_args = [(prediction_set, part, args.output_dir,args) for part in all_parts]
-            # Use a pool of workers to process the files in parallel.
-            with Pool() as pool:
-                pool.starmap(annotate, task_args)
-        except Exception as e:
-            print(f"Error: {e}")
-
-    # Combine all the processed files into one
-    combined_contents = {}
-    json_path = args.output_json
-
-    # Iterate through json files
-    for file_name in os.listdir(output_dir):
-        if file_name.endswith(".json"):
-            file_path = os.path.join(output_dir, file_name)
-            with open(file_path, "r") as json_file:
-                content = json.load(json_file)
-                assert 'pred' in content[0], f"Error: {file_name} don't has key=pred"
-                assert 'score' in content[0], f"Error: {file_name} don't has key=score"
-                combined_contents[file_name[:-5]] = content
-
-    # Write combined content to a json file
-    with open(json_path, "w") as json_file:
-        json.dump(combined_contents, json_file)
-    print("All evaluation completed!")
-
-    class ScoreMeter:
-        def __init__(self):
-            self.score_sum = 0
-            self.count = 0
-            self.yes_count = 0
-            self.no_count = 0
-            self.score_dict = {'yes': defaultdict(int), 'no': defaultdict(int)}
-
-        def add_score(self, score, pred):
-            self.score_sum += score
-            self.count += 1
-            pred_lower = pred.lower()
-            if 'yes' in pred_lower:
-                self.yes_count += 1
-                self.score_dict['yes'][score] += 1
-            elif 'no' in pred_lower:
-                self.no_count += 1
-                self.score_dict['no'][score] += 1
-
-        def get_average_score(self):
-            res = (self.score_sum / self.count) if self.count else 0
-            return f"{res:.6f}"
-
-        def get_accuracy(self, response_type):
-            if response_type == 'yes':
-                res =  (self.yes_count / self.count) if self.count else 0
-            elif response_type == 'no':
-                res = (self.no_count / self.count) if self.count else 0
-            else:
-                res = 0
-            return f"{res:.6f}"
-
-    meter_dic = {'total': ScoreMeter()}
-    for key, result in combined_contents.items():
-        # Computing score
-        score_match = result[0]['score']
-        score = int(score_match)
-        pred = result[0]['pred']
-
-        meter_dic["total"].add_score(score, pred)
-        if 'a_type' in result[1] and result[1]['a_type'] is not None:
-            typ = str(result[1]['a_type'])
-            if typ not in meter_dic:
-                meter_dic[typ] = ScoreMeter()
-            meter_dic[typ].add_score(score, pred)
-
-            if 'next' in args.output_dir:
-                typ = typ[0]
-                if typ not in meter_dic:
-                    meter_dic[typ] = ScoreMeter()
-                meter_dic[typ].add_score(score, pred)
-
-    csv_dic = {'acc': meter_dic["total"].get_accuracy('yes'), 'score': meter_dic["total"].get_average_score()}
-
-    output = ""
-    output += "Yes count: " + str(meter_dic["total"].yes_count) + "\n"
-    output += "No count: " + str(meter_dic["total"].no_count) + "\n"
-    output += "Accuracy: " + str(meter_dic["total"].get_accuracy('yes')) + "\n"
-    output += "Average score: " + str(meter_dic["total"].get_average_score()) + "\n"
-    output += "\n"
-    output += "Total Score Yes/No distribution:\n"
-    for key, value in meter_dic["total"].score_dict.items():
-        output += f"{key}:\n"
-        for k in range(0, 6):
-            v = value[k]
-            output += f"{k}: {v}\n"
-    output += "\n"
-    output += "Answer Type Score distribution:\n"
-    output += 'Type, Accuracy, Avg_score\n'
-    key_list = sorted([k for k in meter_dic.keys()])
-    for key in key_list:
-        output += f"{key}, {meter_dic[key].get_accuracy('yes')}, {meter_dic[key].get_average_score()}\n"
-        csv_dic[key] = meter_dic[key].get_accuracy('yes')
-
-    output += "\n"
-    for k in csv_dic.keys():
-        output += f"{k}, "
-    output = output.rstrip(', ')  # Remove the trailing comma and space
-    output += "\n"
-
-    for k in csv_dic.keys():
-        output += str(csv_dic[k]) + ", "
-    output = output.rstrip(', ')  # Remove the trailing comma and space
-    output += "\n"
-
-    print(output)
-    args.output_csv = args.output_json.replace(".json", ".csv")
-    with open(args.output_csv, 'w') as f:
-        f.write(output)
+    print(batch)
+    #
+    # # Combine all the processed files into one
+    # combined_contents = {}
+    # json_path = args.output_json
+    #
+    # # Iterate through json files
+    # for file_name in os.listdir(output_dir):
+    #     if file_name.endswith(".json"):
+    #         file_path = os.path.join(output_dir, file_name)
+    #         with open(file_path, "r") as json_file:
+    #             content = json.load(json_file)
+    #             assert 'pred' in content[0], f"Error: {file_name} don't has key=pred"
+    #             assert 'score' in content[0], f"Error: {file_name} don't has key=score"
+    #             combined_contents[file_name[:-5]] = content
+    #
+    # # Write combined content to a json file
+    # with open(json_path, "w") as json_file:
+    #     json.dump(combined_contents, json_file)
+    # print("All evaluation completed!")
+    #
+    # class ScoreMeter:
+    #     def __init__(self):
+    #         self.score_sum = 0
+    #         self.count = 0
+    #         self.yes_count = 0
+    #         self.no_count = 0
+    #         self.score_dict = {'yes': defaultdict(int), 'no': defaultdict(int)}
+    #
+    #     def add_score(self, score, pred):
+    #         self.score_sum += score
+    #         self.count += 1
+    #         pred_lower = pred.lower()
+    #         if 'yes' in pred_lower:
+    #             self.yes_count += 1
+    #             self.score_dict['yes'][score] += 1
+    #         elif 'no' in pred_lower:
+    #             self.no_count += 1
+    #             self.score_dict['no'][score] += 1
+    #
+    #     def get_average_score(self):
+    #         res = (self.score_sum / self.count) if self.count else 0
+    #         return f"{res:.6f}"
+    #
+    #     def get_accuracy(self, response_type):
+    #         if response_type == 'yes':
+    #             res =  (self.yes_count / self.count) if self.count else 0
+    #         elif response_type == 'no':
+    #             res = (self.no_count / self.count) if self.count else 0
+    #         else:
+    #             res = 0
+    #         return f"{res:.6f}"
+    #
+    # meter_dic = {'total': ScoreMeter()}
+    # for key, result in combined_contents.items():
+    #     # Computing score
+    #     score_match = result[0]['score']
+    #     score = int(score_match)
+    #     pred = result[0]['pred']
+    #
+    #     meter_dic["total"].add_score(score, pred)
+    #     if 'a_type' in result[1] and result[1]['a_type'] is not None:
+    #         typ = str(result[1]['a_type'])
+    #         if typ not in meter_dic:
+    #             meter_dic[typ] = ScoreMeter()
+    #         meter_dic[typ].add_score(score, pred)
+    #
+    #         if 'next' in args.output_dir:
+    #             typ = typ[0]
+    #             if typ not in meter_dic:
+    #                 meter_dic[typ] = ScoreMeter()
+    #             meter_dic[typ].add_score(score, pred)
+    #
+    # csv_dic = {'acc': meter_dic["total"].get_accuracy('yes'), 'score': meter_dic["total"].get_average_score()}
+    #
+    # output = ""
+    # output += "Yes count: " + str(meter_dic["total"].yes_count) + "\n"
+    # output += "No count: " + str(meter_dic["total"].no_count) + "\n"
+    # output += "Accuracy: " + str(meter_dic["total"].get_accuracy('yes')) + "\n"
+    # output += "Average score: " + str(meter_dic["total"].get_average_score()) + "\n"
+    # output += "\n"
+    # output += "Total Score Yes/No distribution:\n"
+    # for key, value in meter_dic["total"].score_dict.items():
+    #     output += f"{key}:\n"
+    #     for k in range(0, 6):
+    #         v = value[k]
+    #         output += f"{k}: {v}\n"
+    # output += "\n"
+    # output += "Answer Type Score distribution:\n"
+    # output += 'Type, Accuracy, Avg_score\n'
+    # key_list = sorted([k for k in meter_dic.keys()])
+    # for key in key_list:
+    #     output += f"{key}, {meter_dic[key].get_accuracy('yes')}, {meter_dic[key].get_average_score()}\n"
+    #     csv_dic[key] = meter_dic[key].get_accuracy('yes')
+    #
+    # output += "\n"
+    # for k in csv_dic.keys():
+    #     output += f"{k}, "
+    # output = output.rstrip(', ')  # Remove the trailing comma and space
+    # output += "\n"
+    #
+    # for k in csv_dic.keys():
+    #     output += str(csv_dic[k]) + ", "
+    # output = output.rstrip(', ')  # Remove the trailing comma and space
+    # output += "\n"
+    #
+    # print(output)
+    # args.output_csv = args.output_json.replace(".json", ".csv")
+    # with open(args.output_csv, 'w') as f:
+    #     f.write(output)
 
 if __name__ == "__main__":
     main()
