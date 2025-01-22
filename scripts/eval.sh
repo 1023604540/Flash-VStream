70baae4511359a61f4abee181090dcb58742df09
Ziqin Zhang <ziqin.zhang@tum.de>
2025-01-22 20:37:04 +0100

    original eval

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # set up python environment
-conda activate vstream
-
+conda activate vstream1
+export http_proxy=http://proxy:80
+export https_proxy=http://proxy:80
 # set important configurations
 ngpus=8
 gputype=A100
@@ -13,19 +14,23 @@ date_device="$(date +%m%d)_${ngpus}${gputype}"
 
 echo start eval
 # define your openai info here
+OPENAIKEY=""
+OPENAIBASE="https://api.openai.com"
+OPENAITYPE="openai"
+OPENAIVERSION="v1"
 
-for dataset in actnet nextoe msvd msrvtt vsmovienet vsego4d realtime_vsmovienet realtime_vsego4d
+for dataset in msrvtt
 do
     echo start eval ${dataset}
     python -m flash_vstream.eval_video.eval_any_dataset_features \
-        --model-path your_model_checkpoint_path \
+        --model-path /home/vault/b232dd/b232dd16/Flash-VStream/checkpoints-finetune/vstream-7b-finetune-weighted_kmeans1*8-25*4-25*1-Original/checkpoint-5900 \
         --dataset ${dataset} \
         --num_chunks $ngpus \
         --api_key $OPENAIKEY \
         --api_base $OPENAIBASE \
         --api_type $OPENAITYPE \
         --api_version $OPENAIVERSION \
-        --test \
-        >> ${date_device}_vstream-7b-eval-${dataset}.log 2>&1 
+        >> ${date_device}_vstream-7b-eval-${dataset}.log 2>&1
 done
+
 
